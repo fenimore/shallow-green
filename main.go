32157b3ceff489986295cb763a084536f8554db0
polypmer <exorable.ludos@gmail.com>
2016-10-16 22:06:08 -0400

    Added boltdb

--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 	"time"
 
+	"github.com/boltdb/bolt"
 	"github.com/polypmer/ghess"
 )
+
+var games = []byte("games")
 
 type Game struct {
 	g    ghess.Board
@@ -16,9 +20,48 @@ type Game struct {
 // Open Bolddb connection
 
 func main() {
+	db, err := bolt.Open("games.db", 0644, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+	key := []byte("1")
+
+	// Read
+	err = db.View(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket(games)
+		if bucket == nil {
+			return errors.New("No bucket")
+		}
+		val := bucket.Get(key)
+		fmt.Println(string(val))
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Write Put
+
+	value := []byte("hello")
+	err = db.Update(func(tx *bolt.Tx) error {
+		bucket, err := tx.CreateBucketIfNotExists(games)
+		if err != nil {
+			return err
+		}
+		err = bucket.Put(key, value)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	// connection
 	router := NewRouter()
-	err := http.ListenAndServe(":8080", router)
+	err = http.ListenAndServe(":8080", router)
 	if err != nil {
 		fmt.Println(err)
 	}
