e3dd46d33f476ad0b18d01d53bbc2f4535d82d5a
polypmer <exorable.ludos@gmail.com>
2016-10-17 15:04:14 -0400

    Take os?

--- growser.go
+++ growser.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	"github.com/boltdb/bolt"
 )
@@ -46,7 +47,7 @@ func main() {
 	router := NewRouter()
 	//http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("static/img"))))
 	fmt.Println("Serving Chess on :" + *portFlag)
-	err = http.ListenAndServe(":"+*portFlag, router)
+	err = http.ListenAndServe(os.Getenv("PORT"), router)
 	if err != nil {
 		fmt.Println(err)
 	}
